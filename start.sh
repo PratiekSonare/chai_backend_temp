af24c381c14a94477143dc406a85ad2a9fb59374
PratiekSonare <pratiksonare16@gmail.com>
2026-04-01 16:49:47 +0530

    (minor) change to start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-source venv/bin/activate && uvicorn app:app --reload --host 0.0.0.0 --port 5000 2>&1 | tee -a uvicorn.log
+source venv/bin/activate && uvicorn app:app --reload --host 0.0.0.0 --port 5000
 
 # 2>&1 | tee -a uvicorn.log
 
